80d35d929ed5bd98a5e3f251d75508ac5ea0ec48
Josh Simmons <josh@hobbes.hsd1.wa.comcast.net>
2009-03-24 17:13:44 -0700

    It worked! It worked!

--- perlin.cpp
+++ perlin.cpp
@@ -18,34 +18,29 @@ inline GLfloat smoothstep(GLfloat x){
 	return x * x * (3 - 2 * x);
 }
 
-GLfloat **create_noise_map(int w, int h){
-	GLfloat **map = new GLfloat*[h];
+GLfloat *create_noise_map(int w, int h){
+	GLfloat *map = new GLfloat[w*h];
 	int y;
 	for(y = 0; y < h; y++){
-		map[y] = new GLfloat[w];
 		int x;
 		for(x = 0; x < w; x++){
-			map[y][x] = (GLfloat)rand()/(GLfloat)RAND_MAX;
+			map[y*w+x] = (GLfloat)rand()/(GLfloat)RAND_MAX;
 		}
 	}
 	return map;
 }
 
-GLfloat **linear_stretch_map(GLfloat **map, int w, int h, int x_scale, int y_scale){
+GLfloat *linear_stretch_map(GLfloat *map, int w, int h, int x_scale, int y_scale){
 	int x, y;
-	GLfloat **stretched = new GLfloat*[h*y_scale];
+	GLfloat *stretched = new GLfloat[w*x_scale*h*y_scale];
 	for(y = 0; y < h; y++){
-		int ys;
-		for(ys = 0; ys < y_scale; ys++){
-			stretched[y_scale*y+ys] = new GLfloat[w*x_scale];
-		}
 		for(x = 0; x < w; x++){
 			int xs;
-			GLfloat i = map[y][x];
-			GLfloat f = map[y][(x+1)%w];
+			GLfloat i = map[y*w+x];
+			GLfloat f = map[y*w+((x+1)%w)];
 			GLfloat step = (f-i)/(GLfloat)x_scale;
 			for(xs = 0; xs < x_scale; xs++){
-				stretched[y_scale*y][x_scale*x+xs] = i;
+				stretched[y_scale*y*x_scale*w+x_scale*x+xs] = i;
 				i += step;
 			}
 		}
@@ -53,11 +48,11 @@ GLfloat **linear_stretch_map(GLfloat **map, int w, int h, int x_scale, int y_sca
 	for(x = 0; x < w*x_scale; x++){
 		for(y = 0; y < h; y++){
 			int ys;
-			GLfloat i = stretched[y*y_scale][x];
-			GLfloat f = stretched[((y+1)%h)*y_scale][x];
+			GLfloat i = stretched[y*y_scale*w*x_scale+x];
+			GLfloat f = stretched[((y+1)%h)*y_scale*w*x_scale+x];
 			GLfloat step = (f-i)/(GLfloat)y_scale;
 			for(ys = 0; ys < y_scale; ys++){
-				stretched[y*y_scale+ys][x] = i;
+				stretched[(y*y_scale+ys)*w*x_scale+x] = i;
 				i += step;
 			}
 		}
@@ -65,66 +60,55 @@ GLfloat **linear_stretch_map(GLfloat **map, int w, int h, int x_scale, int y_sca
 	return stretched;
 }
 
-GLfloat **smooth_stretch_map(GLfloat **map, int w, int h, int x_scale, int y_scale){
+GLfloat *smooth_stretch_map(GLfloat *map, int w, int h, int x_scale, int y_scale){
 	int x, y;
-	GLfloat **stretched = new GLfloat*[h*y_scale];
+	GLfloat *stretched = new GLfloat[h*y_scale*w*x_scale];
 	for(y = 0; y < h; y++){
-		int ys;
-		for(ys = 0; ys < y_scale; ys++){
-			stretched[y_scale*y+ys] = new GLfloat[w*x_scale];
-		}
 		for(x = 0; x < w; x++){
-			GLfloat i = map[y][x];
-			GLfloat f = map[y][(x+1)%w];
+			GLfloat i = map[y*w+x];
+			GLfloat f = map[y*w+((x+1)%w)];
 			GLfloat step = 1/(GLfloat)x_scale;
 			int xs;
 			for(xs = 0; xs < x_scale; xs++){
-				stretched[y_scale*y][x_scale*x+xs] = i+(f-i)*smoothstep(step*(GLfloat)xs);
+				stretched[y_scale*y*w*x_scale+x_scale*x+xs] = i+(f-i)*smoothstep(step*(GLfloat)xs);
 			}
 		}
 	}
 	for(x = 0; x < w*x_scale; x++){
 		for(y = 0; y < h; y++){
-			GLfloat i = stretched[y*y_scale][x];
-			GLfloat f = stretched[((y+1)%h)*y_scale][x];
+			GLfloat i = stretched[y*y_scale*w*x_scale+x];
+			GLfloat f = stretched[((y+1)%h)*y_scale*w*x_scale+x];
 			GLfloat step = 1/(GLfloat)y_scale;
 			int ys;
 			for(ys = 0; ys < y_scale; ys++){
-				stretched[y*y_scale+ys][x] = i+(f-i)*smoothstep(step*(GLfloat)ys);
+				stretched[(y*y_scale+ys)*w*x_scale+x] = i+(f-i)*smoothstep(step*(GLfloat)ys);
 			}
 		}
 	}
 	return stretched;
 }
 
-GLfloat **perlin_noise(int size, GLfloat persistence, int start, int end){
+GLfloat *perlin_noise(int size, GLfloat persistence, int start, int end){
 	if(end > size || start < 0 || end < start){
 		return NULL;
 	}
 	int s;
 	s = (int)pow((GLfloat)2,size);
-	int i;
-	GLfloat **map = new GLfloat*[s];
-	for(i = 0; i < s; i++){
-		map[i] = new GLfloat[s];
+	GLfloat *map = new GLfloat[s*s];
+	for(int i = 0; i < s*s; i++){
+		map[i] = 0;
 	}
 	GLfloat p = 1, ptot = 0;
-	for(i = (int)pow((GLfloat)2,end); i >= (int)pow((GLfloat)2,start); i/=2){
-		GLfloat **noise_map = create_noise_map(s/i,s/i);
-		GLfloat **temp = smooth_stretch_map(noise_map,s/i,s/i,i,i);
-		for(int j = 0; j < s/i; j++){
-			delete[] noise_map[j];
-		}
+	for(int i = (int)pow((GLfloat)2,end); i >= (int)pow((GLfloat)2,start); i/=2){
+		GLfloat *noise_map = create_noise_map(s/i,s/i);
+		GLfloat *temp = smooth_stretch_map(noise_map,s/i,s/i,i,i);
 		delete[] noise_map;
 		int y;
 		for(y = 0; y < s; y++){
 			int x;
 			for(x = 0; x < s; x++){
-				map[y][x] += temp[y][x]*p;
+				map[y*s+x] += temp[y*s+x]*p;
 			}
-		}
-		for(int j = 0; j < s; j++){
-			delete[] temp[j];
 		}
 		delete[] temp;
 		ptot += p;
@@ -134,28 +118,28 @@ GLfloat **perlin_noise(int size, GLfloat persistence, int start, int end){
 	for(y = 0; y < s; y++){
 		int x;
 		for(x = 0; x < s; x++){
-			map[y][x] /= ptot;
+			map[y*s+x] /= ptot;
 		}
 	}
 	return map;
 }
 
-void cloudify(GLfloat **map, int w, int h, GLfloat cover, GLfloat sharpness){
+void cloudify(GLfloat *map, int w, int h, GLfloat cover, GLfloat sharpness){
 	int y;
 	for(y = 0; y < h; y++){
 		int x;
 		for(x = 0; x < w; x++){
-			GLfloat c = map[y][x] - cover;
+			GLfloat c = map[y*w+x] - cover;
 			if(c < 0){
-				map[y][x] = 0;
+				map[y*w+x] = 0;
 			} else {
-				map[y][x] = 1 - pow(sharpness,c);
+				map[y*w+x] = 1 - pow(sharpness,c);
 			}
 		}
 	}
 }
 
-void to_file(GLfloat **heightmap, int w, int h, string file){
+void to_file(GLfloat *heightmap, int w, int h, string file){
 	if(w > 0 && h > 0){
 		ofstream of(file.c_str());
 		of << "P2 " << w << " " << h << " 255\n";
@@ -164,7 +148,7 @@ void to_file(GLfloat **heightmap, int w, int h, string file){
 		GLfloat min = 0;
 		for(y = 0; y < h; y++){
 			for(x = 0; x < w; x++){
-				of << (int)(255*(heightmap[y][x]-min)/(max-min)) << " ";
+				of << (int)(255*(heightmap[y*w+x]-min)/(max-min)) << " ";
 			}
 			of << "\n";
 		}
@@ -193,7 +177,7 @@ GLuint make_cloud_texture(){
 	//this works well for 256x256 clouds
 	int power = 8;
 	int size = pow((GLfloat)2,power);
-	GLfloat **perlin = perlin_noise(power,.5,0,5);
+	GLfloat *perlin = perlin_noise(power,.5,0,5);
 
 	to_file(perlin, size, size, "perlin.pgm");
 
@@ -206,9 +190,9 @@ GLuint make_cloud_texture(){
 	for(y = 0; y < size; y++){
 		int x;
 		for(x = 0; x < size; x++){
-			tex[y*size*3+x*3+0] = perlin[y][x];
-			tex[y*size*3+x*3+1] = .7+perlin[y][x]*.3;
-			tex[y*size*3+x*3+2] = .8+perlin[y][x]*.2;
+			tex[y*size*3+x*3+0] = perlin[y*size+x];
+			tex[y*size*3+x*3+1] = .7+perlin[y*size+x]*.3;
+			tex[y*size*3+x*3+2] = .8+perlin[y*size+x]*.2;
 		}
 	}
 
@@ -229,9 +213,6 @@ GLuint make_cloud_texture(){
 
 	gluBuild2DMipmaps(GL_TEXTURE_2D,GL_RGB,size,size,GL_RGB,GL_FLOAT,tex);
 
-	for(int i = 0; i < size; i++){
-		delete[] perlin[i];
-	}
 	delete[] perlin;
 	delete[] tex;
 
@@ -242,14 +223,14 @@ GLuint make_ground_texture(){
 	int power = 8;
 	int size = pow((GLfloat)2,power);
 
-	GLfloat **perlin = perlin_noise(power,1.0,0,power);
+	GLfloat *perlin = perlin_noise(power,1.0,0,power);
 	GLfloat *tex = new GLfloat[size*size*3];
 
 	int y;
 	for(y = 0; y < size; y++){
 		int x;
 		for(x = 0; x < size; x++){
-			tex[y*size*3+x*3+0] = tex[y*size*3+x*3+1] = tex[y*size*3+x*3+2] = perlin[y][x];
+			tex[y*size*3+x*3+0] = tex[y*size*3+x*3+1] = tex[y*size*3+x*3+2] = perlin[y*size+x];
 		}
 	}
 
--- perlin.h
+++ perlin.h
@@ -7,14 +7,14 @@
 
 #include <GL/gl.h>
 
-GLfloat **create_noise_map(int w, int h);
+GLfloat *create_noise_map(int w, int h);
 
-GLfloat **linear_stretch_map(GLfloat **map, int w, int h, int x_scale, int y_scale);
-GLfloat **smooth_stretch_map(GLfloat **map, int w, int h, int x_scale, int y_scale);
+GLfloat *linear_stretch_map(GLfloat *map, int w, int h, int x_scale, int y_scale);
+GLfloat *smooth_stretch_map(GLfloat *map, int w, int h, int x_scale, int y_scale);
 
-GLfloat **perlin_noise(int size, GLfloat persistence, int start, int end);
+GLfloat *perlin_noise(int size, GLfloat persistence, int start, int end);
 
-void cloudify(GLfloat **map, int w, int h, GLfloat cover, GLfloat sharpness);
+void cloudify(GLfloat *map, int w, int h, GLfloat cover, GLfloat sharpness);
 
 GLuint make_cloud_texture();
 GLuint make_ground_texture();
